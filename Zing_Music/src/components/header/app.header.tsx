90ed6f496e4cfca074d08f5188ea8c1d4ea81a2a
Huy Tran <baohuy82.th@gmail.com>
2025-08-20 23:35:01 -0700

    Update app.header.tsx

--- Zing_Music/src/components/header/app.header.tsx
+++ Zing_Music/src/components/header/app.header.tsx
@@ -187,15 +187,7 @@ export default function AppHeader() {
           </Search>
           <Box sx={{ flexGrow: 1 }} />
           <Box sx={{ display: { xs: "none", md: "flex" } }}>
-            <IconButton
-              size="large"
-              aria-label="show 4 new mails"
-              color="inherit"
-            >
-              <Badge badgeContent={4} color="error">
-                <MailIcon />
-              </Badge>
-            </IconButton>
+            <span>Playlist</span>
             <IconButton
               size="large"
               aria-label="show 17 new notifications"
