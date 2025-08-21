9a378865567e5c188961875e8ca21be98cc2d842
Huy Tran <baohuy82.th@gmail.com>
2025-08-21 00:16:25 -0700

    Update page.tsx

--- Zing_Music/src/app/page.tsx
+++ Zing_Music/src/app/page.tsx
@@ -2,9 +2,5 @@ import AppHeader from "@/components/header/app.header"
 import App from "next/app"
 
 export default function HomePage() {
-  return (
-    <div>
-      <AppHeader />
-    </div>
-  )
+  return <div>Home Page</div>
 }
