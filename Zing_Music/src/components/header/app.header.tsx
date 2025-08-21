37157b794d9df43b1ce83106da45c8cc0f839dcc
Huy Tran <baohuy82.th@gmail.com>
2025-08-20 23:42:16 -0700

    Update app.header.tsx

--- Zing_Music/src/components/header/app.header.tsx
+++ Zing_Music/src/components/header/app.header.tsx
@@ -52,7 +52,8 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
     transition: theme.transitions.create("width"),
     width: "100%",
     [theme.breakpoints.up("md")]: {
-      width: "20ch",
+      //   width: "20ch",
+      width: "400px",
     },
   },
 }))
