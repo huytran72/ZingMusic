5a3cccfb8962e46525ff1b3ca11e8cc242ed2177
Huy Tran <baohuy82.th@gmail.com>
2025-08-20 23:30:58 -0700

    Update page.tsx

--- Zing_Music/src/app/page.tsx
+++ Zing_Music/src/app/page.tsx
@@ -4,8 +4,6 @@ import App from "next/app"
 export default function HomePage() {
   return (
     <div>
-      <h1>Welcome to Zing Music</h1>
-      <p>Your music streaming experience starts here!</p>
       <AppHeader />
     </div>
   )
