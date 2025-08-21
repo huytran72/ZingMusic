7451f02bf4ac0a3ecf0296a9e5345f432698c072
Huy Tran <baohuy82.th@gmail.com>
2025-08-20 23:50:23 -0700

    Update app.header.tsx

--- Zing_Music/src/components/header/app.header.tsx
+++ Zing_Music/src/components/header/app.header.tsx
@@ -159,7 +159,7 @@ export default function AppHeader() {
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+      <AppBar position="static" sx={{ backgroundColor: "#279b18ff" }}>
         <Container>
           <Toolbar>
             <Typography
