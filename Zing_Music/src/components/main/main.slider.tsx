4d87f067db8d3a1eb738b5d98d2211b6ca26447c
Huy Tran <baohuy82.th@gmail.com>
2025-08-22 00:15:29 -0700

    Update main.slider.tsx

--- Zing_Music/src/components/main/main.slider.tsx
+++ Zing_Music/src/components/main/main.slider.tsx
@@ -75,22 +75,22 @@ const MainSlider = () => {
       <h2>Multiple Tracks</h2>
       <Slider {...settings}>
         <div>
-          <h3>1</h3>
+          <h3>Track 1</h3>
         </div>
         <div>
-          <h3>2</h3>
+          <h3>Track 2</h3>
         </div>
         <div>
-          <h3>3</h3>
+          <h3>Track 3</h3>
         </div>
         <div>
-          <h3>4</h3>
+          <h3>Track 4</h3>
         </div>
         <div>
-          <h3>5</h3>
+          <h3>Track 5</h3>
         </div>
         <div>
-          <h3>6</h3>
+          <h3>Track 6</h3>
         </div>
       </Slider>
       <Divider />
