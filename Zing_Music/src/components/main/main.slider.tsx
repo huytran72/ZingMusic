84beee1a002d1ac04486aecdd7fe23c341e0b708
Huy Tran <baohuy82.th@gmail.com>
2025-08-22 00:11:17 -0700

    Update main.slider.tsx

--- Zing_Music/src/components/main/main.slider.tsx
+++ Zing_Music/src/components/main/main.slider.tsx
@@ -59,26 +59,30 @@ const MainSlider = () => {
   }
 
   return (
-    <Slider {...settings}>
-      <div>
-        <h3>1</h3>
-      </div>
-      <div>
-        <h3>2</h3>
-      </div>
-      <div>
-        <h3>3</h3>
-      </div>
-      <div>
-        <h3>4</h3>
-      </div>
-      <div>
-        <h3>5</h3>
-      </div>
-      <div>
-        <h3>6</h3>
-      </div>
-    </Slider>
+    <Box>
+      <h2>Multiple Tracks</h2>
+      <Slider {...settings}>
+        <div>
+          <h3>1</h3>
+        </div>
+        <div>
+          <h3>2</h3>
+        </div>
+        <div>
+          <h3>3</h3>
+        </div>
+        <div>
+          <h3>4</h3>
+        </div>
+        <div>
+          <h3>5</h3>
+        </div>
+        <div>
+          <h3>6</h3>
+        </div>
+      </Slider>
+      <Divider />
+    </Box>
   )
 }
 
