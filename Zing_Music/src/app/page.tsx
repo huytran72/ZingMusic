81cc404728b9e0e44b8825c235e9aa87c987255b
Huy Tran <baohuy82.th@gmail.com>
2025-08-21 23:26:06 -0700

    Update page.tsx

--- Zing_Music/src/app/page.tsx
+++ Zing_Music/src/app/page.tsx
@@ -1,12 +1,10 @@
-import AppHeader from "@/components/header/app.header"
 import MainSlider from "@/components/main/main.slider"
-import App from "next/app"
-import { Main } from "next/document"
+import { Container } from "@mui/material"
 
 export default function HomePage() {
   return (
-    <div>
+    <Container>
       <MainSlider />
-    </div>
+    </Container>
   )
 }
