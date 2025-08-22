722cebeabb154c2c5ee5db8a0fbf9a9d251c7e35
Huy Tran <baohuy82.th@gmail.com>
2025-08-21 23:40:29 -0700

    Update main.slider.tsx

--- Zing_Music/src/components/main/main.slider.tsx
+++ Zing_Music/src/components/main/main.slider.tsx
@@ -3,6 +3,7 @@ import React from "react"
 import "slick-carousel/slick/slick.css"
 import "slick-carousel/slick/slick-theme.css"
 import Slider from "react-slick"
+import Settings from "react-slick"
 
 const MainSlider = () => {
   const settings = {
