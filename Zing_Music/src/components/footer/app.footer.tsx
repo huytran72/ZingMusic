a644823fff7590fca2ce7c3ecd35a8aa0ff0ed3b
Huy Tran <baohuy82.th@gmail.com>
2025-08-22 00:37:13 -0700

    Update app.footer.tsx

--- Zing_Music/src/components/footer/app.footer.tsx
+++ Zing_Music/src/components/footer/app.footer.tsx
@@ -1,3 +1,4 @@
+"use client"
 import AppBar from "@mui/material/AppBar"
 import AudioPlayer from "react-h5-audio-player"
 import "react-h5-audio-player/lib/styles.css"
@@ -5,7 +6,11 @@ import "react-h5-audio-player/lib/styles.css"
 const AppFooter = () => {
   return (
     <AppBar position="fixed" color="primary" sx={{ top: "auto", bottom: 0 }}>
-      Footer
+      <AudioPlayer
+        src="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-9.mp3"
+        volume={0.5}
+        // Try other props!
+      />
     </AppBar>
   )
 }
