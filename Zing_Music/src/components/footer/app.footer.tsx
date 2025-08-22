b1d3a80b2cc36aea782e3264231b04d6a48b0198
Huy Tran <baohuy82.th@gmail.com>
2025-08-22 00:44:22 -0700

    Update app.footer.tsx

--- Zing_Music/src/components/footer/app.footer.tsx
+++ Zing_Music/src/components/footer/app.footer.tsx
@@ -3,6 +3,7 @@ import { useHasMounted } from "@/utils/customHook"
 import AppBar from "@mui/material/AppBar"
 import AudioPlayer from "react-h5-audio-player"
 import "react-h5-audio-player/lib/styles.css"
+import { Container } from "@mui/material"
 
 const AppFooter = () => {
   const hasMounted = useHasMounted()
@@ -10,11 +11,13 @@ const AppFooter = () => {
   if (!hasMounted) return <></>
   return (
     <AppBar position="fixed" color="primary" sx={{ top: "auto", bottom: 0 }}>
-      <AudioPlayer
-        src="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-9.mp3"
-        volume={0.5}
-        // Try other props!
-      />
+      <Container>
+        <AudioPlayer
+          src="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-9.mp3"
+          volume={0.5}
+          // Try other props!
+        />
+      </Container>
     </AppBar>
   )
 }
