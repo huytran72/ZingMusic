8421493517f6903826875704bae394679aa9fba0
Huy Tran <baohuy82.th@gmail.com>
2025-08-22 00:34:02 -0700

    Update app.footer.tsx

--- Zing_Music/src/components/footer/app.footer.tsx
+++ Zing_Music/src/components/footer/app.footer.tsx
@@ -1,8 +1,10 @@
+import AppBar from "@mui/material/AppBar"
+
 const AppFooter = () => {
   return (
-    <footer>
-      <p>© 2023 Zing Music. All rights reserved.</p>
-    </footer>
+    <AppBar position="fixed" color="primary" sx={{ top: "auto", bottom: 0 }}>
+      Footer
+    </AppBar>
   )
 }
 
