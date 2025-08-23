f599d0fa19b36c3b79c653512ffd7db042eba7fe
Huy Tran <baohuy82.th@gmail.com>
2025-08-22 20:49:22 -0700

    Update page.tsx

--- Zing_Music/src/app/page.tsx
+++ Zing_Music/src/app/page.tsx
@@ -1,19 +1,19 @@
 import MainSlider from "@/components/main/main.slider"
 import { Container } from "@mui/material"
 
-const res = await fetch("http://localhost:8000/api/v1/tracks/top", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify({
-    category: "CHILL",
-    limit: 10,
-  }),
-})
-console.log("Check server:", await res.json())
+export default async function HomePage() {
+  const res = await fetch("http://localhost:8000/api/v1/tracks/top", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      category: "CHILL",
+      limit: 10,
+    }),
+  })
+  console.log("Check server:", await res.json())
 
-export default function HomePage() {
   return (
     <Container>
       <MainSlider />
