62dd14d3c657dcb4ae651c938582758ae3bcd648
Huy Tran <baohuy82.th@gmail.com>
2025-09-04 00:04:10 -0700

    Update page.tsx

--- Zing_Music/src/app/track/[slug]/page.tsx
+++ Zing_Music/src/app/track/[slug]/page.tsx
@@ -1,4 +1,9 @@
+"use client"
+import { useSearchParams } from "next/navigation"
+
 const DetailTrackPage = ({ params }: { params: { slug: string } }) => {
+  const searchParams = useSearchParams()
+
   return <div>DetailTrackPage</div>
 }
 
